d83032b989dabd754fae5332fed92f1efeedadc9
Arun Raghavan <arun@asymptotic.io>
2025-04-29 12:33:23 -0400

    nvim: Drop custom endhints fonts
    Defaults work with cascadia-code nerd fonts installed.

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,12 +44,6 @@ end
 
 -- LSP hints at the end of the line
 require("lsp-endhints").setup {
-	icons = {
-		type = "⬥ ",
-		parameter = "▵ ",
-		offspec = "܀ ", -- hint kind not defined in official LSP spec
-		unknown = "» ", -- hint kind is nil
-	},
 	label = {
 		truncateAtChars = 20,
 		padding = 1,
